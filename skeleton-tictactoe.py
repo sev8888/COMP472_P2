5733d73a00a7b9829427aaf299e3bfb9cdbfef64
Minh Bui <minhbibui@gmail.com>
2021-11-15 20:28:29 -0500

    add heuristic 2

--- skeleton-tictactoe.py
+++ skeleton-tictactoe.py
@@ -33,7 +33,7 @@ class Game:
 		for b in bloc_positions:
 			self.current_state[b[0]][b[1]] = '%'
 		# Player X always plays first
-		self.player_turn = 'X'
+		self.player_tULn = 'X'
 
 	# print board layout
 	def draw_board(self):
@@ -70,101 +70,101 @@ class Game:
 		print("■")
 		print()
 
-	# check if move is valid @return True,False	
+	# check if move is valid @retULn True,False	
 	def is_valid(self, px, py):
 		if px < 0 or px >= n or py < 0 or py >= n:
-			return False
+			retULn False
 		elif self.current_state[px][py] != '.':
-			return False
+			retULn False
 		else:
-			return True
+			retULn True
 
-	# check if game has winner @return 'X','O','.',None
+	# check if game has winner @retULn 'X','O','.',None
 	def is_end(self):
 		# Vertical win
 		for x in range(0,n):  #column
-			current_winner = 'X'
-			current_count = 0 
+			cULrent_winner = 'X'
+			cULrent_count = 0 
 			for y in range (0, n): #row
-				if(self.current_state[x][y] == current_winner):
-					current_count = current_count + 1
-					if((current_winner == 'X' or current_winner == 'O') and current_count >= s):
+				if(self.current_state[x][y] == cULrent_winner):
+					cULrent_count = cULrent_count + 1
+					if((cULrent_winner == 'X' or cULrent_winner == 'O') and cULrent_count >= s):
 						#print("vertical win")
-						return current_winner
+						retULn cULrent_winner
 				else:
-					current_winner = self.current_state[x][y]
-					current_count = 0
+					cULrent_winner = self.current_state[x][y]
+					cULrent_count = 0
 		
 		# Horizontal win
 		for y in range(0,n):  #row
-			current_winner = 'X'
-			current_count = 0 
+			cULrent_winner = 'X'
+			cULrent_count = 0 
 			for x in range (0, n): #column
-				if(self.current_state[x][y] == current_winner):
-					current_count = current_count + 1
-					if((current_winner == 'X' or current_winner == 'O') and current_count >= s):
+				if(self.current_state[x][y] == cULrent_winner):
+					cULrent_count = cULrent_count + 1
+					if((cULrent_winner == 'X' or cULrent_winner == 'O') and cULrent_count >= s):
 						#print("horizontal win")
-						return current_winner
+						retULn cULrent_winner
 				else:
-					current_winner = self.current_state[x][y]
-					current_count = 0
+					cULrent_winner = self.current_state[x][y]
+					cULrent_count = 0
 
 		# Main diagonal win
 		for i in range(-(n-s), n-s+1):
-			current_winner = 'X'
-			current_count = 0 
+			cULrent_winner = 'X'
+			cULrent_count = 0 
 			for j in range(n-abs(i)):
-				if(self.current_state[i+j if i>=0 else 0+j][0+j if i>=0 else abs(i)+j] == current_winner):
-					current_count = current_count + 1
-					if((current_winner == 'X' or current_winner == 'O') and current_count >= s):
+				if(self.current_state[i+j if i>=0 else 0+j][0+j if i>=0 else abs(i)+j] == cULrent_winner):
+					cULrent_count = cULrent_count + 1
+					if((cULrent_winner == 'X' or cULrent_winner == 'O') and cULrent_count >= s):
 						#print("diagonal win")
-						return current_winner
+						retULn cULrent_winner
 				else:
-					current_winner = self.current_state[i+j if i>=0 else 0+j][0+j if i>=0 else abs(i)+j]
-					current_count = 1
-				#print(F'[{0+j if i>=0 else abs(i)+j}][{0+j if i<=0 else i+j}]->{self.current_state[0+j if i>=0 else abs(i)+j][0+j if i<=0 else i+j]}:{current_count}', end="")
+					cULrent_winner = self.current_state[i+j if i>=0 else 0+j][0+j if i>=0 else abs(i)+j]
+					cULrent_count = 1
+				#print(F'[{0+j if i>=0 else abs(i)+j}][{0+j if i<=0 else i+j}]->{self.current_state[0+j if i>=0 else abs(i)+j][0+j if i<=0 else i+j]}:{cULrent_count}', end="")
 
 		# Second diagonal win
 		for i in range(-(n-s), n-s+1):
-			current_winner = 'X'
-			current_count = 0 
+			cULrent_winner = 'X'
+			cULrent_count = 0 
 			for j in range(n-abs(i)):
-				if(self.current_state[i+j if i>=0 else 0+j][(n-1)-j if i>=0 else (n-1)-abs(i)-j] == current_winner):
-					current_count = current_count + 1
-					if((current_winner == 'X' or current_winner == 'O') and current_count >= s):
+				if(self.current_state[i+j if i>=0 else 0+j][(n-1)-j if i>=0 else (n-1)-abs(i)-j] == cULrent_winner):
+					cULrent_count = cULrent_count + 1
+					if((cULrent_winner == 'X' or cULrent_winner == 'O') and cULrent_count >= s):
 						#print("diagonal2 win")
-						return current_winner
+						retULn cULrent_winner
 				else:
-					current_winner = self.current_state[i+j if i>=0 else 0+j][(n-1)-j if i>=0 else (n-1)-abs(i)-j]
-					current_count = 1
+					cULrent_winner = self.current_state[i+j if i>=0 else 0+j][(n-1)-j if i>=0 else (n-1)-abs(i)-j]
+					cULrent_count = 1
 
-		# Is whole board full?
+		# Is whole board fULl?
 		for i in range(0, n):
 			for j in range(0, n):
 				# While there is an empty field, we continue the game
 				if (self.current_state[i][j] == '.'):
-					return None
+					retULn None
 		# If there is no more moves, it's a tie!
-		return '.'
+		retULn '.'
 
-	# print game result
+	# print game resULt
 	def check_end(self):
-		self.result = self.is_end()
+		self.resULt = self.is_end()
 		# Printing the appropriate message if the game has ended
-		if self.result != None:
-			if self.result == 'X':
+		if self.resULt != None:
+			if self.resULt == 'X':
 				print('The winner is X!')
-			elif self.result == 'O':
+			elif self.resULt == 'O':
 				print('The winner is O!')
-			elif self.result == '.':
+			elif self.resULt == '.':
 				print("It's a tie!")
 			self.initialize_game()
-		return self.result
+		retULn self.resULt
 
 	# user move input (x,y)
 	def input_move(self):
 		while True:
-			print(F'Player {self.player_turn}, enter your move:')
+			print(F'Player {self.player_tULn}, enter yoUL move:')
 			#px = int(input('enter the x coordinate: '))
 			strx = input('enter the x coordinate (letter): ')
 			if not strx.isupper():
@@ -176,16 +176,16 @@ class Game:
 			else:
 				py = 100
 			if self.is_valid(px, py):
-				return (px, py)
+				retULn (px, py)
 			else:
 				print('The move is not valid! Try again.')
 
 	def switch_player(self):
-		if self.player_turn == 'X':
-			self.player_turn = 'O'
-		elif self.player_turn == 'O':
-			self.player_turn = 'X'
-		return self.player_turn
+		if self.player_tULn == 'X':
+			self.player_tULn = 'O'
+		elif self.player_tULn == 'O':
+			self.player_tULn = 'X'
+		retULn self.player_tULn
 
 	def minimax(self, max=False):
 		# Minimizing for 'X' and maximizing for 'O'
@@ -199,13 +199,13 @@ class Game:
 			value = -2
 		x = None
 		y = None
-		result = self.is_end()
-		if result == 'X':
-			return (-1, x, y)
-		elif result == 'O':
-			return (1, x, y)
-		elif result == '.':
-			return (0, x, y)
+		resULt = self.is_end()
+		if resULt == 'X':
+			retULn (-1, x, y)
+		elif resULt == 'O':
+			retULn (1, x, y)
+		elif resULt == '.':
+			retULn (0, x, y)
 		for i in range(0, n):
 			for j in range(0, n):
 				if self.current_state[i][j] == '.':
@@ -224,7 +224,7 @@ class Game:
 							x = i
 							y = j
 					self.current_state[i][j] = '.'
-		return (value, x, y)
+		retULn (value, x, y)
 
 	def alphabeta(self, alpha=-2, beta=2, max=False):
 		# Minimizing for 'X' and maximizing for 'O'
@@ -238,13 +238,13 @@ class Game:
 			value = -2
 		x = None
 		y = None
-		result = self.is_end()
-		if result == 'X':
-			return (-1, x, y)
-		elif result == 'O':
-			return (1, x, y)
-		elif result == '.':
-			return (0, x, y)
+		resULt = self.is_end()
+		if resULt == 'X':
+			retULn (-1, x, y)
+		elif resULt == 'O':
+			retULn (1, x, y)
+		elif resULt == '.':
+			retULn (0, x, y)
 		for i in range(0, n):
 			for j in range(0, n):
 				if self.current_state[i][j] == '.':
@@ -265,33 +265,33 @@ class Game:
 					self.current_state[i][j] = '.'
 					if max: 
 						if value >= beta:
-							return (value, x, y)
+							retULn (value, x, y)
 						if value > alpha:
 							alpha = value
 					else:
 						if value <= alpha:
-							return (value, x, y)
+							retULn (value, x, y)
 						if value < beta:
 							beta = value
-		return (value, x, y)
+		retULn (value, x, y)
 
 	def e1(self, score, depth, player='X'):
 		# fast algorithm
 		# player is maximizing
 		# opponent is minimizing
-		# heuristic counts number of other X/O placed in the the same line
+		# heUListic counts number of other X/O placed in the the same line
 		if player == 'X':
 			opponent = 'O'
 		else:
 			opponent = 'X'
 
-		result = self.is_end()
-		if result == player:
-			return (1000, x, y)
-		elif result == opponent:
-			return (-1000, x, y)
-		elif result == '.':
-			return (0, x, y)
+		resULt = self.is_end()
+		if resULt == player:
+			retULn (1000, x, y)
+		elif resULt == opponent:
+			retULn (-1000, x, y)
+		elif resULt == '.':
+			retULn (0, x, y)
 		
 		score = 0
 		scoref = score
@@ -338,15 +338,15 @@ class Game:
 						scoref = score
 						x = i
 						y = j
-		return (scoref, x, y)
+		retULn (scoref, x, y)
 
 	def e2(self,first_player,second_player, negative_infinitive):
 		# initialize the possible_move dictionary 
-		possible_move = {'UL' : 0, 'U' : 0, 'UR' : 0, 'R' : 0, 'L' : 0, 'DL' : 0, 'D' : 0, 'DR' : 0}
+		possible_move = {'UL' : 0, 'U' : 0, 'UL' : 0, 'R' : 0, 'L' : 0, 'DL' : 0, 'D' : 0, 'DR' : 0}
 		# position x of posible move
-		x_posible_move = {'UL' : 0, 'U' : 0, 'UR' : 0, 'R' : 0, 'L' : 0, 'DL' : 0, 'D' : 0, 'DR' : 0}
+		x_posible_move = {'UL' : 0, 'U' : 0, 'UL' : 0, 'R' : 0, 'L' : 0, 'DL' : 0, 'D' : 0, 'DR' : 0}
 		# position y of possible move
-		y_posible_move = {'UL' : 0, 'U' : 0, 'UR' : 0, 'R' : 0, 'L' : 0, 'DL' : 0, 'D' : 0, 'DR' : 0}
+		y_posible_move = {'UL' : 0, 'U' : 0, 'UL' : 0, 'R' : 0, 'L' : 0, 'DL' : 0, 'D' : 0, 'DR' : 0}
 		best_score = negative_infinitive
 		chosen_position =""
 		# first player choice
@@ -356,7 +356,7 @@ class Game:
 				for j in range(0,n):
 					# if the location is empty
 					if self.current_state[i][j] == '.':
-						# checking 8 surronding position 
+						# checking 8 sULronding position 
 						# if possible direction is player 2 => - infinitive
 						# if possible next direction is player1, move next and add +1
 						# if possible next direction = empty => +1
@@ -430,9 +430,107 @@ class Game:
 											y_posible_move['U'] = x-1
 						
 							# check for moving up - left
+							if (i-1) >= 0 and (j+1) < n:
+								x_posible_move['UL'] = i
+								y_posible_move['UL'] = j 
+								a = i 
+								b = j 
+								flag = True
+								while flag:
+									a = a - 1
+									b = b + 1
+									# if it reaches the end assign flag to false
+									if a <= 0 or b >= n-1:
+										flag = False
+									#if empty add 1 point to posible move
+									if self.current_state[a][b] == '.':
+										possible_move['UL'] = possible_move['UL'] + 1
+									#if there is player 2 add negative_infinitive to posible move
+									elif self.current_state[a][b] == second_player:
+										possible_move['UL'] = possible_move['UL'] + negative_infinitive
+									# if there is player 1 add 1 and move next
+									elif self.current_state[a][b] == first_player:
+										possible_move['UL'] = possible_move['UL'] + 1
+										# moving to the left because there is already a value in that specific location
+										x_posible_move['UL'] = a
+										y_posible_move['UL'] = b
+									
+
 							# check for moving up - right
+							if (i-1) >= 0 and (j-1) >= 0:
+								x_posible_move['UR'] = i
+								y_posible_move['UR'] = j 
+								a = i 
+								b = j 
+								flag = True
+								while flag:
+									a = a - 1
+									b = b - 1
+									# if it reaches the end assign flag to false
+									if a <= 0 or b <= 0:
+										flag = False
+									#if empty add 1 point to posible move
+									if self.current_state[a][b] == '.':
+										possible_move['UR'] = possible_move['UR'] + 1
+									#if there is player 2 add negative_infinitive to posible move
+									elif self.current_state[a][b] == second_player:
+										possible_move['UR'] = possible_move['UR'] + negative_infinitive
+									# if there is player 1 add 1 and move next
+									elif self.current_state[a][b] == first_player:
+										possible_move['UR'] = possible_move['UR'] + 1
+										# moving to the left because there is already a value in that specific location
+										x_posible_move['UR'] = a
+										y_posible_move['UR'] = b
 							# check for moving down - left
+							if (i+1) < n and (j-1) >= 0:
+								x_posible_move['DL'] = i
+								y_posible_move['DL'] = j 
+								a = i 
+								b = j 
+								flag = True
+								while flag:
+									a = a + 1
+									b = b - 1
+									# if it reaches the end assign flag to false
+									if a >= n or b <= 0:
+										flag = False
+									#if empty add 1 point to posible move
+									if self.current_state[a][b] == '.':
+										possible_move['DL'] = possible_move['DL'] + 1
+									#if there is player 2 add negative_infinitive to posible move
+									elif self.current_state[a][b] == second_player:
+										possible_move['DL'] = possible_move['DL'] + negative_infinitive
+									# if there is player 1 add 1 and move next
+									elif self.current_state[a][b] == first_player:
+										possible_move['DL'] = possible_move['DL'] + 1
+										# moving to the left because there is already a value in that specific location
+										x_posible_move['DL'] = a
+										y_posible_move['DL'] = b
 							# check for moving down - right
+							if (i+1) < n and (j+1) < n:
+								x_posible_move['DR'] = i
+								y_posible_move['DR'] = j 
+								a = i 
+								b = j 
+								flag = True
+								while flag:
+									a = a + 1
+									b = b + 1
+									# if it reaches the end assign flag to false
+									if a >= n or b >= n:
+										flag = False
+									#if empty add 1 point to posible move
+									if self.current_state[a][b] == '.':
+										possible_move['DR'] = possible_move['DR'] + 1
+									#if there is player 2 add negative_infinitive to posible move
+									elif self.current_state[a][b] == second_player:
+										possible_move['DR'] = possible_move['DR'] + negative_infinitive
+									# if there is player 1 add 1 and move next
+									elif self.current_state[a][b] == first_player:
+										possible_move['DR'] = possible_move['DR'] + 1
+										# moving to the left because there is already a value in that specific location
+										x_posible_move['DR'] = a
+										y_posible_move['DR'] = b
 			# checking which move is the best
 			for key in possible_move:
 				if(possible_move[key] > best_score):
@@ -448,30 +546,29 @@ class Game:
 			player_x = self.HUMAN
 		if player_o == None:
 			player_o = self.HUMAN
-
 		while True:
 			self.draw_board()
 			if self.check_end():
-				return
+				retULn
 			if player_x == self.AI or player_o == self.AI or self.recommend: #only run ais when necessary
 				start = time.time()
 				if algo == self.MINIMAX:
-					if self.player_turn == 'X':
+					if self.player_tULn == 'X':
 						(_, x, y) = self.minimax(max=False)
 					else:
 						(_, x, y) = self.minimax(max=True)
 				elif algo == self.ALPHABETA:
-					if self.player_turn == 'X':
+					if self.player_tULn == 'X':
 						(m, x, y) = self.alphabeta(max=False)
 					else:
 						(m, x, y) = self.alphabeta(max=True)
 				elif algo == self.H1:
-					if self.player_turn == 'X':
+					if self.player_tULn == 'X':
 						(_, x, y) = self.e1(aggression=2)
 					else:
 						(_, x, y) = self.e1(player='O', aggression=2)
 				end = time.time()
-			if (self.player_turn == 'X' and player_x == self.HUMAN) or (self.player_turn == 'O' and player_o == self.HUMAN):
+			if (self.player_tULn == 'X' and player_x == self.HUMAN) or (self.player_tULn == 'O' and player_o == self.HUMAN):
 					if self.recommend:
 						real_x = alphabet_upper[x]
 						with open(dir,'a') as f:
@@ -482,15 +579,15 @@ class Game:
 						print(F'Recommended move: x = {real_x}, y = {y}')
 						
 					(x,y) = self.input_move()
-			if (self.player_turn == 'X' and player_x == self.AI) or (self.player_turn == 'O' and player_o == self.AI):
+			if (self.player_tULn == 'X' and player_x == self.AI) or (self.player_tULn == 'O' and player_o == self.AI):
 					real_x = alphabet_upper[x]
 					with open(dir,'a') as f:
 						
-						f.writelines(F'Player {self.player_turn} under AI control plays: {real_x}{y}\n')
+						f.writelines(F'Player {self.player_tULn} under AI control plays: {real_x}{y}\n')
 						f.writelines(F'i	Evaluation time: {round(end - start, 7)}s\n')
 					print(F'Evaluation time: {round(end - start, 7)}s')
-					print(F'Player {self.player_turn} under AI control plays: x = {real_x}, y = {y}')
-			self.current_state[x][y] = self.player_turn
+					print(F'Player {self.player_tULn} under AI control plays: x = {real_x}, y = {y}')
+			self.current_state[x][y] = self.player_tULn
 			self.switch_player()
 
 def main():
@@ -505,7 +602,7 @@ def main():
 		g.play(algo=sel,player_x=Game.AI,player_o=Game.AI)
 
 if __name__ == "__main__":
-	#user inputs for game configuration
+	#user inputs for game configULation
 	alphabet_upper = list(string.ascii_uppercase)
 	bloc_positions=[]
 	print("Hello, welcome to the CLI\n")
@@ -557,13 +654,13 @@ if __name__ == "__main__":
 	print("====  the maximum depth of adversarial search for player 2\n")
 	d2 = int(input())
 
-	print("==== the maximum time allowed (in seconds) for the program to return a move\n")
+	print("==== the maximum time allowed (in seconds) for the program to retULn a move\n")
 	t = float(input())
 
 	print("==== to force the use of minimax input  0, to force the use of alphabeta input 1\n")
 	sel = bool(input())
 
-	print("==== select the player configuration from the following options:\n"+
+	print("==== select the player configULation from the following options:\n"+
 	"\t1 for Human vs Human\n" + "\t2 for Human vs AI (Human is player X)\n" + "\t3 for AI vs Human (Human is player o)\n" + "\t4 for AI vs AI")
 	modes = int(input())
 	while(modes > 4 or modes < 1):
@@ -571,8 +668,8 @@ if __name__ == "__main__":
 		modes = int(input())
 
 	filename = "gamefile"+str(n)+str(b)+str(s)+str(t)+".txt"	
-	path_current = os.getcwd()
-	path_new = path_current+"/game_files"
+	path_cULrent = os.getcwd()
+	path_new = path_cULrent+"/game_files"
 	dir = os.path.join(path_new, filename)
 	#os.mkdir(path_new)
 	if not os.path.exists(path_new):
@@ -587,7 +684,7 @@ if __name__ == "__main__":
 		f.writelines("the value of the board size n is "+str(n)+"\n")
 		f.writelines("the number of blocs b is "+str(b)+"\n")
 		f.writelines("the value of the winning line-up s is "+str(s)+"\n")
-		f.writelines("the value of the maximum allowed time for the program to return a move is "+str(t)+"\n")
+		f.writelines("the value of the maximum allowed time for the program to retULn a move is "+str(t)+"\n")
 		f.writelines("\nposition of blocs:\n")
 		if(b>0):
 			for i in bloc_positions:
